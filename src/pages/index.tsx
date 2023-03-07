4467ea69607f4798df53f53c4e21113194094dd0
Guilherme-Zobel <guilherme.zobel@gmail.com>
2023-03-07 09:57:22 -0300

    style: componente Countdown

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,18 +4,24 @@ import { Inter } from 'next/font/google'
 import styles from '../styles/pages/Home.module.css'
 import Head from 'next/head'
 import { CompletedChallenges } from '@/components/CompletedChallenges'
+import { Countdown } from '@/components/Countdown'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
+
       <ExperienceBar />
 
       <section>
         <div>
           <Profile />
           <CompletedChallenges />
+          <Countdown />
         </div>
         <div>
 
